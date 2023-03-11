e9a1996fd61769f1368cd725f17e1b820a534d18
Jesiara <jesiarakhatun2000@gmail.com>
2023-03-11 18:28:17 +0600

    I have created sample2 java class

---
+++ app/src/main/java/com/example/navigationdrawerjava/sample2.java
@@ -0,0 +1,29 @@
+package com.example.navigationdrawerjava;
+
+public class sample2 {
+    String name,email;
+    public sample2(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public sample2() {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
